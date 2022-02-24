9435a6428966bc5c172198fd29fdefc17acce518
Pisit Wajanasara <compisit1234@gmail.com>
2022-02-23 16:01:18 -0800

    Implement vulnerable server demo

---
+++ vulnerable_server/README.md
@@ -0,0 +1,35 @@
+# Simple Log4Shell vulnerable server
+
+This is a simple web server which use vulnerable Log4j version. It was written in Java programming language and use Javalin as the web framework.
+
+## Dependencies
+
+* Javalin 4.3.0
+* Log4j 2.5.0
+* Slf4j 1.8.0 beta 4
+
+
+## Requirement
+
+* Java 11 or greater
+
+## Building project
+
+For windows using PowerShell
+
+```sh
+.\mvnw.cmd assembly:assembly
+```
+
+For unix based system
+```sh
+./mvnw assembly:assembly
+```
+
+The compile artifact should be in `target/vulnerable_server-1.0-SNAPSHOT-jar-with-dependencies.jar`
+
+## Running server
+
+```
+java -jar vulnerable_server-1.0-SNAPSHOT-jar-with-dependencies.jar
+```
---
+++ vulnerable_server/src/main/java/com/nekoteoj/vulnerableserver/App.java
@@ -0,0 +1,26 @@
+package com.nekoteoj.vulnerableserver;
+
+import io.javalin.Javalin;
+import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.core.config.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main(String[] args) {
+        Configurator.initialize(new DefaultConfiguration());
+        Configurator.setRootLevel(Level.INFO);
+        Javalin app = Javalin.create().start(3000);
+        app.get("/", ctx -> {
+            String userAgent = ctx.header("User-Agent");
+            logger.info("Client's user agent: " + userAgent);
+            ctx.result("Hello, World");
+        });
+    }
+
+}
